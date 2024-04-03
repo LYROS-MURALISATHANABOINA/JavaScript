abde8d91424c3679ed34ed190553467a07b8e5cd
npx <smuralikrishna@lyrostech.com>
2024-04-03 12:40:50 +0530

    first commit

---
+++ pract1.js
@@ -0,0 +1,34 @@
+marks = [
+    { subject: "Maths", marks: 45 },
+    { subject: "Science", marks: 90 },
+    { subject: "English", marks: 15 },
+    { subject: "EVS", marks: 55 }
+  ];
+// console.log(marks[0].subject)
+//   marks.map((val,index) =>{
+//     console.log(val.subject+" "+val.marks)
+//   })
+
+//   let x=marks.filter((val,index) =>{
+//     return val.marks>45
+//   })
+//   console.log(x)
+
+// let x=marks.reduce((total,val) =>{
+//     return total+val.marks
+//   },0)
+//   console.log(x)
+
+// let x=marks.reduce((total,val) =>{
+//     return total+val.marks
+//   },0)
+//   console.log(x)
+
+// for(let i=0;i<marks.length;i++){
+//     if(marks[i].marks<20){
+//         marks[i].marks+=10;
+//     }
+// }
+// marks.map((val)=>{
+//     console.log(val.subject+"  "+val.marks)
+// })
---
+++ pract3.js
@@ -0,0 +1,11 @@
+const a=(v2)=>{
+    console.log(v2)
+}
+const b=(v1) =>{
+console.log(v1)
+}
+const start =(a,b,v1,v2) =>{
+    a(v1)
+    b(v2)
+}
+start(a,b,1)
